1b600c4b807baba0a438d74c19b3c1b78e433df6
Marcin Lika <lika.marcin@gmail.com>
2019-12-17 21:24:11 +0100

    create User class

---
+++ User.php
@@ -0,0 +1,60 @@
+<?php
+
+class User
+{
+    private $id;
+    
+    private $name;
+    
+    private $surname;
+    
+    private $email;
+    
+    private $password;
+    
+    public function getId()
+    {
+        return $this->id;
+    }
+    
+    public function getName()
+    {
+        return $this->name;
+    }
+    
+    public function getSurname()
+    {
+        return $this->surname;
+    }
+    
+    public function getEmail()
+    {
+        return $this->email;
+    }
+    
+    public function getPassword()
+    {
+        return $this->password;
+    }
+    
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+    
+    public function setSurname($surname)
+    {
+        $this->surname = $surname;
+    }
+    
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+    
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+}
+
