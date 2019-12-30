cf7f82a73629b6e844ccfe03ea9b316b5937f5be
Marcin Lika <lika.marcin@gmail.com>
2019-12-30 15:10:26 +0100

    Update Tweet.php and Comment.php

--- Comment.php
+++ Comment.php
@@ -1,14 +1,40 @@
 <?php
 
-require_once './Tweet.php';
-
-class Comment extends Tweet
+class Comment
 {
+    private $id;
+    
+    private $userId;
+    
     private $tweetId;
+    
+    private $commentBody;
+    
+    private $creationDate;
+    
+    public function getId()
+    {
+        return $this->id;
+    }
+    
+    public function getUserId()
+    {
+        return $this->userId;
+    }
     
     public function getTweetId()
     {
         return $this->tweetId;
+    }
+    
+    public function getCommentBody()
+    {
+        return $this->commentBody;
+    }
+    
+    public function getCreationDate()
+    {
+        return $this->creationDate;
     }
 }
 
