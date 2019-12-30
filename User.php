e245364c7df79b42f502478e85562885b4f5ebc0
Marcin Lika <lika.marcin@gmail.com>
2019-12-30 14:45:44 +0100

    User name, surname, email validation

--- User.php
+++ User.php
@@ -39,17 +39,30 @@ class User
     
     public function setName($name)
     {
-        $this->name = $name;
+        if (is_string($name) && strlen($name) > 1) {
+            $this->name = filter_var($name, FILTER_SANITIZE_STRING);
+        } else {
+            throw new Exception('User name must be a string');
+        }
+        
     }
     
     public function setSurname($surname)
     {
-        $this->surname = $surname;
+        if (is_string($surname) && strlen($surname) > 1) {
+            $this->surname = filter_var($surname, FILTER_SANITIZE_STRING);
+        } else {
+            throw new Exception('User surname must be a string');
+        }
     }
     
     public function setEmail($email)
     {
-        $this->email = $email;
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $this->email = $email;
+        } else {
+            throw new Exception('Invalid email');
+        }
     }
     
     public function setPassword($password)
@@ -58,3 +71,8 @@ class User
     }
 }
 
+$user = new User();
+$user->setEmail('marcin@wp.pl');
+$user->setName('adam');
+var_dump($user->getEmail());
+var_dump($user->getName());
